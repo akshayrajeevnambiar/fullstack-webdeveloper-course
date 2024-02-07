5f44ffca1d76dce2c15374040dd29e12c9c58bad
Akshay-Rajeev-Nambiar <akshayrajeevnambiar@gmail.com>
2024-02-06 20:47:45 -0500

    implemented the counter with setInterval

--- week 2/01-aync/easy/1-counter.js
+++ week 2/01-aync/easy/1-counter.js
@@ -4,13 +4,15 @@ Create a counter in JavaScript
 We have already covered this in the second lesson, but as an easy recap try to code a counter in Javascript
 It should go up as time goes by in intervals of 1 second
 */
-
-function clock() {
-  const timer = new Date();
-  return timer.toLocaleTimeString();
+function counter(start, end) {
+  let intervalID = setInterval(() => {
+    let currentCount = start++;
+    console.clear();
+    console.log(currentCount);
+    if (start > end) {
+      clearInterval(intervalID);
+    }
+  }, 1000);
 }
 
-setInterval(() => {
-  console.clear();
-  console.log(clock());
-}, 1000);
+counter(1, 10);
